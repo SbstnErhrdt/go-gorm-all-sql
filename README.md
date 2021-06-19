65202eed0aa94f1d7a223563316b59466453f24d
Seb <git@erhardt.net>
2021-06-19 16:54:43 +0200

    add logrus

--- README.md
+++ README.md
@@ -6,11 +6,10 @@ This is a generic connector interface for the following databases:
 * Postgres
 * SQLite
 
-With this connector, it is easy to run your sql based golang software,
-written with the gorm.io framework,
-on different databases, by only changing one environment variable.
+With this connector, it is easy to run your sql based golang software, written with the gorm.io framework, on different
+databases, by only changing one environment variable.
 
-Just set `SQL_TYPE` to either `MYSQL`, `POSTGRES`, `SQLITE` and you are good to go. 
+Just set `SQL_TYPE` to either `MYSQL`, `POSTGRES`, `SQLITE` and you are good to go.
 
 ## Installation
 
@@ -19,24 +18,28 @@ go get github.com/SbstnErhrdt/go-gorm-all-sql
 ```
 
 ## Examples
+
 ```go
 package main
+
 import (
-    "github.com/SbstnErhrdt/go-gorm-all-sql/pkg/sql"
+	"github.com/SbstnErhrdt/go-gorm-all-sql/pkg/sql"
 )
 
-func main()  {
-    sql.ConnectToDatabase()
+func main() {
+	sql.ConnectToDatabase()
 }
 ```
 
-
 ## Todo
+
 * Tests
 * Docker compose environment
 
 ## MySQL
+
 Env variables
+
 ```
 SQL_HOST=
 SQL_USER=
@@ -46,6 +49,7 @@ SQL_DATABASE=
 ```
 
 ## Postgres
+
 ```
 SQL_HOST=
 SQL_USER=
@@ -54,12 +58,23 @@ SQL_PORT=
 SQL_DATABASE=
 SQL_SSL= // optional
 ```
+
 ## SQLite
+
 ```
 SQL_DATABASE=
 ```
 
+## Logger
+
+to add the logrus logger, add the following environment flag
+
+```
+LOGGER=LOGRUS
+```
+
 ## Functionality
+
 * Works with different SQL dialects
 * Checks if necessary environment variables are present
 * Retries multiple times, if there is no connection available
--- pkg/sql/connect.go
+++ pkg/sql/connect.go
@@ -2,11 +2,12 @@ package sql
 
 import (
 	"errors"
+	"github.com/SbstnErhrdt/go-gorm-all-sql/pkg/logrus_logger"
 	"github.com/SbstnErhrdt/go-gorm-all-sql/pkg/mysql"
 	"github.com/SbstnErhrdt/go-gorm-all-sql/pkg/postgres"
 	"github.com/SbstnErhrdt/go-gorm-all-sql/pkg/sqlite"
+	log "github.com/sirupsen/logrus"
 	"gorm.io/gorm"
-	"log"
 	"os"
 	"strings"
 	"time"
@@ -23,7 +24,7 @@ func ConnectToDatabaseWithConfig(config *gorm.Config) (client *gorm.DB, err erro
 	return connect(config)
 }
 
-// getSQlType extracts the sql type environment flag from the environment
+// getSQlType extracts the sql type environment variable from the environment
 // if there is no flag present the fallback is MYSQL
 func getSQlType() string {
 	// Get env variables
@@ -34,12 +35,28 @@ func getSQlType() string {
 	return strings.ToUpper(sqlType)
 }
 
+// getLogger extracts the logger environment variable from the environment
+// if there is no flag present the fallback is DEFAULT
+func getLogger() string {
+	// Get env variables
+	loggerType := os.Getenv("LOGGER")
+	if len(loggerType) == 0 {
+		loggerType = "DEFAULT"
+	}
+	return strings.ToUpper(loggerType)
+}
+
 var FailedConnectionCounter = 10 // amount of retries to connect to the database
 
 // connect Tries to connect to the database
 // Does this recursively until there is a connection
 // or until the counter is 0
 func connect(config *gorm.Config) (client *gorm.DB, err error) {
+	// read the logger information from the environment
+	switch getLogger() {
+	case "LOGRUS":
+		config.Logger = logrus_logger.New()
+	}
 	switch getSQlType() {
 	case "MYSQL":
 		client, err = mysql.ConnectToMysql(config)
@@ -51,12 +68,12 @@ func connect(config *gorm.Config) (client *gorm.DB, err error) {
 		err = errors.New("can not find this sql type")
 		return
 	}
-	log.Println("InitSqlClient: type:", getSQlType())
+	log.Info("InitSqlClient: type:", getSQlType())
 	// if there are problems with connecting to the database
 	// retry
 	if err != nil {
 		// If we can not connect we try again in 10 seconds
-		log.Println("InitSqlClient: error:", err)
+		log.Error("InitSqlClient: error:", err)
 		FailedConnectionCounter--
 		// Base case
 		if FailedConnectionCounter == 0 {
@@ -65,7 +82,7 @@ func connect(config *gorm.Config) (client *gorm.DB, err error) {
 			log.Fatal("InitSQLClient: FATAL", err)
 			return
 		}
-		log.Println("InitSqlClient: retry")
+		log.Info("InitSqlClient: retry")
 		time.Sleep(time.Second * 5)
 		// Retry again
 		return connect(config)
